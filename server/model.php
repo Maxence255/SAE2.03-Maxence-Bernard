989507a40dae2acdc8a1f34664f01fbc55bee1b1
Maxence Bernard <maxence.bernard@etu.unilim.fr>
2025-04-03 10:38:30 +0200

    maj credentials bdd

--- server/model.php
+++ server/model.php
@@ -14,6 +14,6 @@
  * DBPWD : Mot de passe pour se connecter à la base de données.
  */
 define("HOST", "localhost");
-define("DBNAME", "mora");
-define("DBLOGIN", "root");
-define("DBPWD", "root");
+define("DBNAME", "bernard196");
+define("DBLOGIN", "bernard196");
+define("DBPWD", "bernard196");
